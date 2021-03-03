e6228cbe4b06fc0c0083efdd53ac6b2df819ce51
Lucas santos <lucas.santos@corp.evosystems.com.br>
2021-03-03 00:49:02 -0300

    form control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
+import { rootRouterConfig } from './app.routes';
+import { RouterModule } from '@angular/router';
+import { SobreComponent } from './Reactive-Forms/institucional/sobre/sobre/sobre.component';
+import { FooterComponent } from './Reactive-Forms/navegacao/footer/footer/footer.component';
+import { HomeComponent } from './Reactive-Forms/navegacao/home/home/home.component';
+import { MenuComponent } from './Reactive-Forms/navegacao/menu/menu/menu.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RxjsComponent } from './Observables/component/rxjs.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RxjsComponent
+    RxjsComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    [RouterModule.forRoot(rootRouterConfig, {useHash: false})]
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
