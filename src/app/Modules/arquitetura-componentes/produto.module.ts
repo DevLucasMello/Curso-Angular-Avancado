83eaa63794cd5af965ceee1fe38e1cfc6adcc59b
Lucas santos <lucas.santos@corp.evosystems.com.br>
2021-03-05 00:22:21 -0300

    atualização

--- src/app/Modules/arquitetura-componentes/produto.module.ts
+++ src/app/Modules/arquitetura-componentes/produto.module.ts
@@ -1,9 +1,9 @@
+import { ProdutoRoutingModule } from './produto.route';
+import { ProdutosDashboardComponent } from './produtos-dashboard/produtos-dashboard.component';
 import { ProdutoCountComponent } from './componentes/produto-count/produto-count.component';
 import { ProdutoCardDetalheComponent } from './componentes/produto-card-detalhe/produto-card-detalhe.component';
 import { CommonModule, registerLocaleData } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { ProdutosDashboardComponent } from "./produtos-dashboard/produtos-dashboard.component";
-import { ProdutoRoutingModule } from "./produto.route";
 import  localePt from '@angular/common/locales/pt';
 registerLocaleData(localePt);
 
