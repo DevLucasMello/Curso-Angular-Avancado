bea8c8fdf39dc5c9ee10eb7e6b6e3db78be2c5e2
DevLucasMello <69225219+DevLucasMello@users.noreply.github.com>
2021-03-08 16:29:47 -0300

    Observables (#5)
    * promise
    
    * finalizado observable
    
    * form control
    
    * Reactive-Forms
    
    Co-authored-by: Lucas santos <lucas.santos@corp.evosystems.com.br>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
+import { CadastroComponent } from './Reactive-Forms/cadastro/cadastro.component';
+import { rootRouterConfig } from './app.routes';
+import { RouterModule } from '@angular/router';
+import { SobreComponent } from './Reactive-Forms/institucional/sobre/sobre/sobre.component';
+import { FooterComponent } from './Reactive-Forms/navegacao/footer/footer/footer.component';
+import { HomeComponent } from './Reactive-Forms/navegacao/home/home/home.component';
+import { MenuComponent } from './Reactive-Forms/navegacao/menu/menu/menu.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RxjsComponent } from './Observables/component/rxjs.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RxjsComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    SobreComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    [RouterModule.forRoot(rootRouterConfig, {useHash: false})]
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
